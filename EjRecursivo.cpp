567b88360e4904904d421a33ed79eaa3f9b36d37
Bruno Parada <brunoparadaalcantara2007@gmail.com>
2026-03-02 13:11:14 -0400

    Refactor menu options and add prime sum function

--- EjRecursivo.cpp
+++ EjRecursivo.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-
 int factorial(int n);
 bool esPrimo(int n);
-void tablaMulti(int n);
+int sumaPrimos(int n);
+void tablaMulti(int n, int i = 1);
 
 int main() {
     int opcion;
 
     do {
-        cout << "1) Factorial" << endl;
-        cout << "2) Verifica si un numero es primo" << endl;
+        cout << "\n1) Factorial" << endl;
+        cout << "2) Suma de numeros primos hasta N" << endl;
         cout << "3) Tabla de multiplicar" << endl;
         cout << "4) Cuentame un chiste" << endl;
         cout << "0) Salir" << endl;
@@ -19,9 +19,10 @@ int main() {
         cin >> opcion;
 
         switch (opcion) {
+
         case 1: {
             int num;
-            cout << "Ingrese el numero a calcular el factorial: ";
+            cout << "Ingrese el numero para calcular el factorial: ";
             cin >> num;
             cout << "El factorial de " << num << " es: " << factorial(num) << endl;
             break;
@@ -29,12 +30,10 @@ int main() {
 
         case 2: {
             int num;
-            cout << "Ingrese un numero para verificar si es primo: ";
+            cout << "Ingrese un numero N para sumar los primos hasta N: ";
             cin >> num;
-            if (esPrimo(num))
-                cout << "El numero " << num << " es primo." << endl;
-            else
-                cout << "El numero " << num << " NO es primo." << endl;
+            cout << "La suma de numeros primos hasta " << num
+                << " es: " << sumaPrimos(num) << endl;
             break;
         }
 
@@ -45,9 +44,10 @@ int main() {
             tablaMulti(num);
             break;
         }
+
         case 4: {
-            cout << "Que le dice un jardinero a otro? " << endl;
-            cout << " - Nos vemos cuando podamos " << endl;
+            cout << "Que le dice un jardinero a otro?\n";
+            cout << " - Nos vemos cuando podamos \n";
             break;
         }
 
@@ -56,17 +56,18 @@ int main() {
             break;
 
         default:
-            cout << "Opcion invalida|" << endl;
+            cout << "Opcion invalida" << endl;
         }
 
     } while (opcion != 0);
 
     return 0;
 }
- 
+
+
 int factorial(int n) {
     if (n < 0)
-        return -1; 
+        return -1;
     if (n == 0 || n == 1)
         return 1;
     return n * factorial(n - 1);
@@ -75,18 +76,27 @@ int factorial(int n) {
 bool esPrimo(int n) {
     if (n <= 1)
         return false;
-
-    for (int i = 2; i <= n / 2; i++) {
+    for (int i = 2; i * i <= n; i++) {
         if (n % i == 0)
             return false;
     }
     return true;
 }
 
+int sumaPrimos(int n) {
+    if (n < 2)
+        return 0;
+    if (esPrimo(n))
+        return n + sumaPrimos(n - 1);
+    else
+        return sumaPrimos(n - 1);
+}
 
-void tablaMulti(int n) {
-    cout << "Tabla del " << n << endl;
-    for (int i = 1; i <= 10; i++) {
-        cout << n << " x " << i << " = " << n * i << endl;
-    }
+
+
+void tablaMulti(int n, int i) {
+    if (i > 10)
+        return;
+    cout << n << " x " << i << " = " << n * i << endl;
+    tablaMulti(n, i + 1);
 }
